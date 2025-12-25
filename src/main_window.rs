36fd53f1cdb3cc45992c0162f2bacf1cd6ff6923
Michael Büsch <m@bues.ch>
2025-12-25 22:02:05 +0100

    Change license from GPL to MIT/Apache

--- src/main_window.rs
+++ src/main_window.rs
@@ -2,19 +2,7 @@
 //
 // Copyright 2021 Michael Buesch <m@bues.ch>
 //
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License along
-// with this program; if not, write to the Free Software Foundation, Inc.,
-// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+// SPDX-License-Identifier: MIT OR Apache-2.0
 //
 
 mod game_meta_view;
@@ -37,19 +25,7 @@ const ABOUT_TEXT: &str = "Wolfsmühle - Board game\n\
      \n\
      Copyright 2021 Michael Buesch <m@bues.ch>\n\
      \n\
-     This program is free software; you can redistribute it and/or modify\n\
-     it under the terms of the GNU General Public License as published by\n\
-     the Free Software Foundation; either version 2 of the License, or\n\
-     (at your option) any later version.\n\
-     \n\
-     This program is distributed in the hope that it will be useful,\n\
-     but WITHOUT ANY WARRANTY; without even the implied warranty of\n\
-     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n\
-     GNU General Public License for more details.\n\
-     \n\
-     You should have received a copy of the GNU General Public License along\n\
-     with this program; if not, write to the Free Software Foundation, Inc.,\n\
-     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.";
+     SPDX-License-Identifier: MIT OR Apache-2.0";
 
 pub struct MainWindow {
     appwindow: gtk::ApplicationWindow,
